a3d430d76f84e260a7612cb4d41c87054d9c8323
arzoo98 <52541972+arzoo98@users.noreply.github.com>
2019-08-14 00:07:40 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,10 @@
+import { songs } from '../../app/songInfo';
+
+
+export const  FAVSONGS: songs[] = [
+    {songName: 'What do you mean', artist:'Justin Beiber', genre:'pop', releaseYear:2015, picture:'./assets/images//justin.png'},
+    {songName: 'Senorita', artist:'Shawn Mendes & Camila Cabello', genre:'latin', releaseYear:2019, picture:'./assets/images/senorita.jpg'},
+    {songName: 'Havana', artist:'Camila Cabello', genre:'pop', releaseYear:2017, picture:'./assets/images/havana.jpg'},
+    {songName: 'Perfect', artist:'Ed Sheeran', genre:'classical crossover', releaseYear:2017, picture:'./assets/images/perfect.jpg'},
+];
+
